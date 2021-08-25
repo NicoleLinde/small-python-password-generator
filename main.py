96c1bc0f02a5452ae639c4c77ba84b1dbc5ed632
NicoleLinde <76264833+NicoleLinde@users.noreply.github.com>
2021-08-25 15:43:06 +0000

    deleted comments

--- main.py
+++ main.py
@@ -28,9 +28,3 @@ for number_amount in range(0, nr_numbers):
 
 final_password=''.join(random.sample(password, len(password)))
 print(final_password) 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-
-
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
